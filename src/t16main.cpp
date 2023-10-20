8614256fbf4a0ba2509d078d421f131d356802d5
theamk <code@theamk.com>
2023-10-19 23:26:47 -0400

    somewhat usable firmware
    can count up or down, depending on timer value
    at start.
    
    green button is for start/stop time, fully works
    - flashes once per second when ticking
    - blinks continously when time expired
    - on in time set mode
    
    blue button is for setup, not implemented yet

--- src/t16main.cpp
+++ src/t16main.cpp
@@ -121,78 +121,31 @@ void set_speaker_freq(int freq) {
   last_speaker_freq = freq;
 }
 
-/*
-// display demo
-void loop() {  
-  delay(50);
-  digitalWrite(LED_BUILTIN, (step & 15) > 3);
-  static int step = 0, x = 0, y = 0;
-  step++;
-
-  setpixel(y, x, mode);
-  x+=1;
-  if (x >= DISP_WIDTH) {
-    x=0; y++;
-    if (y >= DISP_HEIGHT) {
-      y = 0; mode = !mode;
-    }
-  }
-  display_update();
-}
-*/
-
-int step = 0;
-unsigned long next_tick = 0;
-int last_encoder = 0;
 
 enum {
-  M_TICK,
-  M_EXPIRED,
-  M_SET_TIME,
-  M_BOOTUP
-} mode = M_BOOTUP;
+  T_TICK_UP,
+  T_TICK_DOWN,
+  T_EXPIRED,
+  T_SET_TIME,
+} timer_mode = T_SET_TIME;
+
+enum {
+  M_TIME,  // default, time is shown
+  M_SET_STEP,
+} menu_mode = M_TIME;
+  
 
 // timer value, seconds
-int time_left = 120;
-
-unsigned long tone_state = 0;
-
+int time_left = 0;
+// millis() timestamp when this changes
+unsigned long next_tick = 0;
+// how many seconds does each pixel represent
 int pixel_time = 10;
 
-void loop() {
 
-  // our encoder gives us 4 counts per detent
-  int this_encoder = encoder.getCount() / 4;
-  int delta_encoder = this_encoder - last_encoder;
-  last_encoder = this_encoder;
-
-  bool green_down = !digitalRead(PIN_BTN_GREEN);
-  bool blue_down = !digitalRead(PIN_BTN_BLUE);
-
-  bool refresh = false;
- 
-  if (green_down) {
-    if (mode != M_TICK) {
-      next_tick = millis();
-    }
-    mode = M_TICK;
-    refresh = true;
-    power_off_time = 0;    
-  } else if (blue_down) {
-    mode = M_SET_TIME;
-    refresh = true;
-    power_off_time = 0;    
-  };
-    
-  digitalWrite(PIN_LED_GREEN, (mode == M_TICK));
-  digitalWrite(PIN_LED_BLUE, (mode == M_SET_TIME));
-
-  // just in case, turn speaker off
+unsigned long tone_state = 0;
+void maybe_update_speaker() { 
   if (!tone_state) {
-    set_speaker_freq(0);
-  } else if (delta_encoder || blue_down || green_down) {
-    // any input turns tone off
-    tone_state = 0;
     set_speaker_freq(0);
   } else { // play the tone
     unsigned int elapsed = millis() - tone_state;
@@ -202,7 +155,7 @@ void loop() {
     
     if ((elapsed % 300) < 50) { set_speaker_freq(0); }
     else {
-      int num = elapsed / 300;
+      unsigned int num = elapsed / 300;
       if (num < (sizeof(melody)/sizeof(melody[0]))) {
         set_speaker_freq(melody[num]);
       } else {
@@ -211,11 +164,91 @@ void loop() {
       };
     }
   }
+}
+
+
+void handle_buttons() {
+  // debounce green
+  static unsigned long green_down_since = 0;
+  bool green_down = false;
+  if (digitalRead(PIN_BTN_GREEN)) {
+    green_down_since = 0;
+  } else if (!green_down_since) {
+    green_down_since = millis();
+  } else if (millis() < (green_down_since + 50)) {
+    green_down = true;
+  }
+
+  static unsigned long blue_down_since = 0;
+  bool blue_down = false;
+  if (digitalRead(PIN_BTN_BLUE)) {
+    blue_down_since = 0;
+  } else if (!blue_down_since) {
+    blue_down_since = millis();
+  } else if (millis() < (blue_down_since + 50)) {
+    blue_down = true;
+  }
+
+  static bool green_was_down = false;
+  if (green_down && !green_was_down) {
+    // green button controls timer mode
+    switch(timer_mode) {
+    case T_EXPIRED:
+    case T_TICK_UP:
+    case T_TICK_DOWN: // was ticking -> pause, set time
+      timer_mode = T_SET_TIME;
+      menu_mode = M_TIME;
+      break;
+    case T_SET_TIME:
+      if (time_left == 0) {
+        timer_mode = T_TICK_UP;
+      } else {
+        timer_mode = T_TICK_DOWN;
+      }
+      menu_mode = M_TIME;
+      next_tick = millis();
+      break;
+    }
+  };
+  green_was_down = green_down;
   
-  // input handling
-  switch (mode) {
-  case M_SET_TIME:
-    if (delta_encoder) {
+  static bool blue_was_down = false;
+  if (blue_down && !blue_was_down) {
+    // blue button controls menu mode
+    switch (menu_mode) {
+    case M_TIME: menu_mode = M_SET_STEP; break;
+    default:
+      menu_mode = M_TIME;
+    }
+  };
+  blue_was_down = blue_down;
+
+  if (green_down || blue_down) {
+    power_off_time = 0; // any input resets power-off timer
+    tone_state = 0; // any input turns tone off
+  }
+}
+
+
+void handle_encoder() {
+  // our encoder gives us 4 counts per detent
+  int this_encoder = encoder.getCount() / 4;
+  static int last_encoder = 0;
+  int delta_encoder = this_encoder - last_encoder;
+  last_encoder = this_encoder;
+
+  if (delta_encoder == 0) {
+    return;
+  }
+  power_off_time = 0;  
+  tone_state = 0; // any input turns tone off
+    
+  switch (menu_mode) {
+  case M_SET_STEP:
+    // TODO
+    break;
+  default:
+    if (timer_mode == T_SET_TIME) {
       while (delta_encoder < 0) {
         delta_encoder++;
         time_left = ((time_left - 1) / pixel_time) * pixel_time;
@@ -225,120 +258,148 @@ void loop() {
         delta_encoder--;
         time_left = ((time_left / pixel_time) + 1) * pixel_time;
       }
-      refresh = true;
-      power_off_time = 0;
-    }
-
-    if (!power_off_time) {
-      power_off_time = millis() + IDLE_POWER_OFF_INTERVAL;
-    } else if (power_off_time < millis()) {
-      digitalWrite(PIN_POWER_OFF, 1);
-      // should not reach here, but in case it didn't work for some reason, reset.
-      delay(100);
-      digitalWrite(PIN_POWER_OFF, 0);
-      power_off_time = 0;
     }
     break;
-  case M_TICK:
+  }
+}
+
+
+void update_leds() {   
+  unsigned long partial = (millis() + 2000 - next_tick) % 1000;
+  switch (timer_mode) {
+  case T_TICK_UP:
+  case T_TICK_DOWN: 
+    digitalWrite(PIN_LED_GREEN, partial < 100); // blink  once per second
+    break;
+  case T_EXPIRED:
+    digitalWrite(PIN_LED_GREEN, (partial % 250) < 125); // blink quickly nonstop
+    break;
+  case T_SET_TIME:
+    digitalWrite(PIN_LED_GREEN, true);
+  }
+
+  digitalWrite(PIN_LED_BLUE, (menu_mode != M_TIME));
+}
+
+
+void update_maybe_power_off() {
+  if (timer_mode == T_TICK_UP || timer_mode == T_TICK_DOWN) {
+    // if timer is ticking or inputs, don't power off
+    power_off_time = 0;
+  } else if (!power_off_time) {
+    // activity happened, reset timer
+    power_off_time = millis() + IDLE_POWER_OFF_INTERVAL;
+  } else if (power_off_time < millis()) {
+    // power off time
+    digitalWrite(PIN_POWER_OFF, 1);    
+    // should not reach here, but in case it didn't work for some reason, reset.
+    delay(100);
+    digitalWrite(PIN_POWER_OFF, 0);
+    power_off_time = 0;
+  }
+}  
+
+
+void maybe_refresh_256px_display() {
+  int pixels = (time_left + pixel_time - 1) / pixel_time;
+
+  if (timer_mode == T_TICK_DOWN && ((time_left % pixel_time) == 1)) {
+    // last pixel blinks before disappearing
+    unsigned long partial = 1000 - (millis() + 2000 - next_tick) % 1000;    
+    if ((partial > 50 && partial < 100) ||
+        (partial > 150 && partial < 200)) {
+      pixels--;
+    }
+  };
+  
+  static int last_pixels = -1;
+  if (pixels != last_pixels) {
+    set_spiral_pixels(pixels);
+    last_pixels = pixels;
+  };
+}
+
+void maybe_refresh_7seg_display() {    
+  char buff[20] = {};
+  if (time_left < 3600) {
+    // less than 1 hour -> show minutes + seconds, 2 digits
+    sprintf(buff, "%02d%02d", (time_left / 60), (time_left % 60));
+  } else {
+    // more than 1 hour -> show hours + minutes, 1 digit
+    sprintf(buff, "%2d%02d", (time_left / 3600), ((time_left / 60)% 60));
+  }
+  if (timer_mode == T_SET_TIME && power_off_time) {
+    // blink on idle input
+    unsigned long idle = (millis()  + IDLE_POWER_OFF_INTERVAL - power_off_time );
+    if ((idle > 500) & ((idle % 500) > 250)) {
+      sprintf(buff, "      ");
+    }
+  }
+  bool colon = (timer_mode != T_TICK_UP) || (timer_mode != T_TICK_DOWN) || (time_left & 1);
+
+  // Force change invisible character because there is a bug: display won't refresh if
+  // only colon changes but string stays the same.
+  buff[4] = colon ? ':' : ' ';
+  buff[5] = 0;
+
+  static String last_display = "ZZZZZZ";
+  if (last_display == buff) {
+    return;
+  }
+  last_display = String(buff);
+  
+  // Re-enable pins if they were disabled before (see below)
+  pinMode(CLOCK_DIO, OUTPUT);    
+  pinMode(CLOCK_CLK, OUTPUT);
+  delay(1); 
+    
+  clock_disp.setBrightness(7); // 0..7
+  if (colon) {
+    clock_disp.colonOn();
+  } else {
+      clock_disp.colonOff();
+  }
+  clock_disp.display(last_display);
+
+  // We want to tri-state control pins between runs, to enable soft shut-off
+  // the TM1637 library we have uses "mI2C.h" driver, which uses regular digitalWrite
+  // calls. So we can just tri-state the pins.
+  pinMode(CLOCK_CLK, INPUT_PULLUP);
+  pinMode(CLOCK_DIO, INPUT);
+}
+
+
+void loop() {
+  handle_buttons();
+  handle_encoder();
+
+  update_leds();
+  maybe_update_speaker();
+  
+  // tick handling
+  switch (timer_mode) {
+  case T_SET_TIME:
+  case T_EXPIRED:
+    break;
+  case T_TICK_UP:
+  case T_TICK_DOWN:
     if (next_tick < millis()) {
       next_tick += 1000;
-      time_left -= 1;
-      if (time_left == 0) {
-        mode = M_EXPIRED;
-        tone_state = millis(); // start music playback
+      if (timer_mode == T_TICK_UP) {
+        time_left++;
+      } else {
+        time_left--;
+        if (time_left == 0) {
+          timer_mode = T_EXPIRED;
+          tone_state = millis(); // start music playback
+        }
       }
-      refresh = true;
     }
-    power_off_time = 0;
-    break;
-  case M_EXPIRED:
-    break;
-  case M_BOOTUP:
-    refresh = true;
-    mode = M_SET_TIME;
     break;
   }
-      
 
-  // display handling
-  if (refresh) {    
-    int pixels = (time_left + pixel_time - 1) / pixel_time;
-    if ((time_left % pixel_time) == 1 || ((time_left % pixel_time) == 3)) pixels--;
-    set_spiral_pixels(pixels);
-    
-    char buff[20] = {};
-    if (time_left < 0) {
-      sprintf(buff, "-%d%02d", (-time_left / 60), (-time_left % 60));
-    } else if (time_left < 3600) {
-      // less than 1 hour -> show minutes + seconds, 2 digits
-      sprintf(buff, "%02d%02d", (time_left / 60), (time_left % 60));
-    } else {
-      // more than 1 hour -> show hours + minutes, 1 digit
-      sprintf(buff, "%2d%02d", (time_left / 3600), ((time_left / 60)% 60));
-    }
-
-    // Re-enable pins if they were disabled before (see below)
-    pinMode(CLOCK_DIO, OUTPUT);    
-    pinMode(CLOCK_CLK, OUTPUT);
-    delay(1); 
-    
-    clock_disp.setBrightness(7); // 0..7
-    if ((time_left & 1) || (mode != M_TICK)) {
-      clock_disp.colonOn();
-      // Force change invisible character because there is a bug: display won't refresh if
-      // only colon changes but string stays the same.
-      buff[4] = '.';
-    } else {
-      clock_disp.colonOff();
-      buff[4] = ' ';
-    }
-    buff[5] = 0;
-    clock_disp.display(String(buff));
-
-    // We want to tri-state control pins between runs, to enable soft shut-off
-    // the TM1637 library we have uses "mI2C.h" driver, which uses regular digitalWrite
-    // calls. So we can just tri-state the pins.
-    pinMode(CLOCK_CLK, INPUT_PULLUP);
-    pinMode(CLOCK_DIO, INPUT);
-
-  }
+  update_maybe_power_off();
   
-
-  /*
-  // update exactly 4 times per second
-  if (millis() < next_update) { return; }
-  next_update += 250;
-  
-  digitalWrite(LED_BUILTIN, (step & 15) > 3);
-  step++;
-  int time = display_count_on();  
-  if (display_count_on() == 0) {
-    delay(200);
-    for (int i=0; i<4; i++) {
-      display_set_all(i & 1);
-      display_update();
-      delay(100);
-    }
-    set_random_pixels(120);
-  }
-
-  if ((step % 4) == 0) {
-    int x, y;
-    // pick random dot until we find active one.
-    // TODO make more efficient
-    while (1) {
-      x = random() % DISP_WIDTH;
-      y = random() % DISP_HEIGHT;
-      if (setpixel(x, y, false)) break;
-    }
-    display_update();
-    // blink it
-    for (int i=0; i<4; i++) {
-      delay(50);
-      setpixel(x, y, (i & 1) == 0);
-      display_update();
-    }
-  }
-  */
-  
-}
+  maybe_refresh_7seg_display();
+  maybe_refresh_256px_display();
+}    
