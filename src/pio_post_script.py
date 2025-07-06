8b2bc925570c2fb0c54672e14992f2262a738102
theamk <code@theamk.com>
2025-07-05 23:39:13 -0400

    Generate .lst file during build process

--- src/pio_post_script.py
+++ src/pio_post_script.py
@@ -11,3 +11,14 @@ def copyout_uf2(source, target, env):
     shutil.copy(uf2_file, os.path.basename(uf2_file))
 
 env.AddPostAction("${PROGPATH}", copyout_uf2)
+
+
+# Create .lst file during build process, based on
+# https://community.platformio.org/t/how-to-create-a-lst-file-using-objdump-during-the-build-process/34632/5
+env.AddPostAction(
+    "$BUILD_DIR/${PROGNAME}.elf",
+    env.VerboseAction(" ".join([
+        "arm-none-eabi-objdump", "-d", "-S", "--demangle",
+        "$BUILD_DIR/${PROGNAME}.elf", ">", "$BUILD_DIR/${PROGNAME}.lst"
+    ]), "Building $BUILD_DIR/${PROGNAME}.lst")
+)
